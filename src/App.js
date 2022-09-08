66b9089c3cc82326b53729859cbd913c9da50440
KhizarLaique <khizarsafilaique@gmail.com>
2022-09-08 12:31:41 +0500

    BS

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>React First App</p>
+        <p class="Display-6">React First App</p>
       </header>
     </div>
   );
